45ad037b7e9d8533ba3676a895fc818ffa3916e4
128500 <kudin-aleksandr@mail.ru>
2017-07-23 23:56:00 +0300

    23.07.2017

---
+++ cwp/src/main/java/com/llisovichok/models/Base.java
@@ -0,0 +1,17 @@
+package com.llisovichok.models;
+
+/**
+ * Created by KUDIN ALEKSANDR on 21.07.2017.
+ */
+public abstract class Base {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
